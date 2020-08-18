9534421e91f4062c2784d0eb547fc13689f64e18
kyle Lee <skeleton7206172@gmail.com>
2020-08-18 19:56:23 +0800

    login ok

--- cs2020-web/src/main/java/com/fg/web/controller/BaseController.java
+++ cs2020-web/src/main/java/com/fg/web/controller/BaseController.java
@@ -6,5 +6,6 @@ import org.slf4j.LoggerFactory;
 public class BaseController {
 	
 	protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+	protected String goPage = "";
 
 }
--- cs2020-web/src/main/java/com/fg/web/controller/Intro.java
+++ cs2020-web/src/main/java/com/fg/web/controller/Intro.java
@@ -2,13 +2,16 @@ package com.fg.web.controller;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fg.web.utils.CookiesUtil;
+import com.fg.web.vo.UserInfo;
 
 @Controller
 public class Intro extends BaseController {
@@ -18,13 +21,13 @@ public class Intro extends BaseController {
 
 	@RequestMapping("/烘培雞")
 	public String goHomePage(HttpServletRequest request, @RequestParam(value = "title", required = false, defaultValue = "GO") String title, Model model) {
-		String goPage = "page/index";
+		goPage = "page/index";
 		
 		Cookie ckLogInfo = CookiesUtil.getCookieByName(request, "userLogInfo");
 		if(ckLogInfo == null) {
-			goPage = "testLogin";
-			return goPage;
-		} 
+			logger.info(">>> no user info");
+			return "page/login";
+		}
 		
 		Cookie ckJSESSIONID = CookiesUtil.getCookieByName(request, "JSESSIONID");
 		logger.info(">>Jid:" + ckJSESSIONID.getValue());
@@ -38,11 +41,32 @@ public class Intro extends BaseController {
 	}
 	
 	@RequestMapping("/doLogin")
-	public String login(HttpServletRequest request, @RequestParam(value = "title", required = false, defaultValue = "GO") String title, Model model) {
+	public String doLogin(HttpServletResponse res , Model model, 
+			@RequestParam(value = "useracc", required = true) String useracc, 
+			@RequestParam(value = "userpsw", required = true) String userpsw) {
 		
+		logger.info(">>>useracc : " + useracc);
+		logger.info(">>>userpsw : " + userpsw);
 		
+		//輸入錯誤，踢回LOGIN
+		if(StringUtils.pathEquals(useracc, "") || StringUtils.pathEquals(userpsw, "")) {
+			return "page/login";
+		}
 		
-		return "";
+		//TOFO: 資料庫檢核
+		if( !StringUtils.pathEquals(useracc, USERACCOUNT) || !StringUtils.pathEquals(userpsw, USERPASSWORD)) {
+			return "page/login";
+		}
+		
+		//無誤，處理後允許登入
+		CookiesUtil.setCookie(res, "userLogInfo", useracc + userpsw, 300);
+		UserInfo userInfo = new UserInfo();
+		userInfo.setUseracc(useracc);
+		userInfo.setUserpsw(userpsw);
+		model.addAttribute("userInfo", userInfo);
+		goPage = "page/index";
+		
+		return goPage;
 	}
 	
 	
